b15dc3356ddcffffc63f00ad8e8b164beea7ed10
Christopher Treviño <christophertrev@gmail.com>
2015-01-22 20:00:37 -0800

    (feat) Add 404 page

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-
+app.get('*', function (req, res){
+  res.status(404).send('You messed up buddy')
+})
 
 var server = app.listen(3000, function () {
 
