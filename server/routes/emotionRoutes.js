f974e6129cbc5bbe0d16c4e1fe1e71b6ac20995f
Christopher Treviño <christophertrev@gmail.com>
2015-01-23 09:44:32 -0800

    (feat) Add randomness to get files

--- server/routes/emotionRoutes.js
+++ server/routes/emotionRoutes.js
@@ -8,19 +8,28 @@ var happyFaceFileName = path.resolve(happyFaceFileName);
 var sadFaceFileName = __dirname + '/../img/sad/sad-puppy.jpg'
 var sadFaceFileName = path.resolve(sadFaceFileName);
 
-router.get('/happy', function (req,res){
-  console.log(happyFaceFileName)
-  // res.send('emtions you made it!')
-  var pathName = path.resolve(__dirname + '/../img/happy/')
-  fs.readdir(pathName,function (err,files){
+var baseURL = __dirname + '/../img/';
+var emotionDirs = {
+  happy: path.resolve(baseURL + 'happy'),
+  sad: path.resolve(baseURL + 'sad'),
+}
+
+
+router.get('/:emotion',function (req,res){
+  // console.log(emotionDirs[req.params.emotion]);
+  var pathDirName = emotionDirs[req.params.emotion];
+  //if emotion in emotions dictionaray do the thing
+  //else send 404 not found
+  fs.readdir(pathDirName,function (err,files){
     //fiels include .DS_STORE
     console.log(files)
     console.log(files.length)
-    var rIndex = Math.ceil(Math.random()*(files.length-1));
 
-    var filePathName = pathName +'/'+ files[rIndex];
-    console.log('werw',filePathName)
-    //join two files
+    var randFloat = Math.random()*(files.length); 
+
+    var rIndex = Math.floor(randFloat);
+    console.log(randFloat)
+    var filePathName = pathDirName +'/'+ files[rIndex];
     res.sendFile(filePathName, {}, function (err){
       if(err){
         console.log(err);
@@ -29,28 +38,50 @@ router.get('/happy', function (req,res){
       } else {
         console.log('Sent:',filePathName)
       }
-
     })
-
-
   });
+})
 
-}) 
+
+// router.get('/happy', function (req,res){
+//   console.log(happyFaceFileName)
+//   // res.send('emtions you made it!')
+//   var pathName = path.resolve(__dirname + '/../img/happy/')
+//   fs.readdir(pathName,function (err,files){
+//     //fiels include .DS_STORE
+//     // console.log(files)
+//     // console.log(files.length)
+//     var rIndex = Math.ceil(Math.random()*(files.length-1));
+
+//     var filePathName = pathName +'/'+ files[rIndex];
+//     // console.log('werw',filePathName)
+
+//     res.sendFile(filePathName, {}, function (err){
+//       if(err){
+//         console.log(err);
+//         res.status(err.status).end();
+
+//       } else {
+//         console.log('Sent:',filePathName)
+//       }
+//     })
+//   });
+// }) 
 
 
-router.get('/sad', function (req,res){
-  console.log(sadFaceFileName)
-  // res.send('emtions you made it!')
-  res.sendFile(sadFaceFileName, {}, function (err){
-    if(err){
-      console.log(err);
-      res.status(err.status).end()
-    } else {
-      console.log('Sent:',sadFaceFileName)
-    }
+// router.get('/sad', function (req,res){
+//   console.log(sadFaceFileName)
+//   // res.send('emtions you made it!')
+//   res.sendFile(sadFaceFileName, {}, function (err){
+//     if(err){
+//       console.log(err);
+//       res.status(err.status).end()
+//     } else {
+//       console.log('Sent:',sadFaceFileName)
+//     }
 
-  })
-}) 
+//   })
+// }) 
 
 
 
