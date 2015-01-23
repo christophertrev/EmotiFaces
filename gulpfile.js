82946e414d2a05ee986083d3ddaf27fd70d7a555
Christopher Treviño <christophertrev@gmail.com>
2015-01-22 19:55:52 -0800

    (gulp) Add nodemon and mocha for testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var nodemon = require('gulp-nodemon');
+var mocha = require('gulp-mocha');
 
 var paths = {
   scripts: [
@@ -13,9 +15,24 @@ var paths = {
   }
 };
 
-
 gulp.task('lint', function() {
   return gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('mon',function (){
+  nodemon({ 
+    script:'server/server.js',
+    ext: 'js'
+  })
+  .on('change', ['lint'])
+  .on('restart', function (){
+    console.log('restarting baby');
+  });
+});
+
+gulp.task('test', function () {
+    return gulp.src(paths.server.specs, {read: false})
+        .pipe(mocha({reporter: 'nyan'}));
 });
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+
+
 var server = app.listen(3000, function () {
 
   var host = server.address().address;
