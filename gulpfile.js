be66f18c9a6b67444959058e9fa86735545cce96
Christopher Treviño <christophertrev@gmail.com>
2015-01-22 19:06:16 -0800

    (refactor) Add paths object in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,20 @@ var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
+var paths = {
+  scripts: [
+  './server/*.js',
+  './gulpfile.js'
+  ],
+  server : {
+    js: ['./server/*.js'],
+    specs: ['.server/test/*.js']
+  }
+};
+
+
 gulp.task('lint', function() {
-  return gulp.src('./server/*.js')
+  return gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
