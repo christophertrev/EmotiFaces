e94dfcc5f252076bcfb7f27440b3f74298a1a2db
Christopher Treviño <christophertrev@gmail.com>
2015-02-10 21:07:23 -0800

    (Feat) position loading pacman in middle

--- client/lib/bundle.js
+++ client/lib/bundle.js
@@ -252,21 +252,33 @@ var EmotionList = React.createClass({displayName: "EmotionList",
 // "https://avatars3.githubusercontent.com/u/6379188?v=3&s=460"
 
   render: function(){
-    console.log('in image rendering', this.props)
-    var imageSRC = 'http://emotifaces.herokuapp.com/emotion/'
-    if(this.props.selectedID){
-      imageSRC += this.props.allEmotions[this.props.selectedID].emotion
-    } else {
-      imageSRC = '../img/loading.gif'
-    }
+    // console.log('in image rendering', this.props)
+    // var imageSRC = 'http://emotifaces.herokuapp.com/emotion/'
+    // if(this.props.selectedID){
+    //   imageSRC += this.props.allEmotions[this.props.selectedID].emotion
+    // } else {
+    //   imageSRC = '../img/loading.gif'
+    // }
     //console.log(this.props.im)
     // console.log(imageSRC)
     return (
+      React.createElement("div", {
+      className: cx({
+        'picture': true
+      }) 
+      }, 
+      React.createElement("img", {
+      className: cx({
+        'loadingImg': true
+      }), 
+      src: "img/loading.gif"}), 
+
       React.createElement("img", {
       className: cx({
         'emotionImg': true
       }), 
       src: this.props.imgSRC})
+      )
     )
   }
 
--- client/src/components/image.js
+++ client/src/components/image.js
@@ -6,21 +6,33 @@ var EmotionList = React.createClass({
 // "https://avatars3.githubusercontent.com/u/6379188?v=3&s=460"
 
   render: function(){
-    console.log('in image rendering', this.props)
-    var imageSRC = 'http://emotifaces.herokuapp.com/emotion/'
-    if(this.props.selectedID){
-      imageSRC += this.props.allEmotions[this.props.selectedID].emotion
-    } else {
-      imageSRC = '../img/loading.gif'
-    }
+    // console.log('in image rendering', this.props)
+    // var imageSRC = 'http://emotifaces.herokuapp.com/emotion/'
+    // if(this.props.selectedID){
+    //   imageSRC += this.props.allEmotions[this.props.selectedID].emotion
+    // } else {
+    //   imageSRC = '../img/loading.gif'
+    // }
     //console.log(this.props.im)
     // console.log(imageSRC)
     return (
+      <div
+      className = {cx({
+        'picture': true
+      }) }      
+      >
+      <img 
+      className= {cx({
+        'loadingImg': true
+      })} 
+      src='img/loading.gif' />
+
       <img
       className = {cx({
         'emotionImg': true
       }) }
       src={this.props.imgSRC}/>
+      </div>
     )
   }
 
