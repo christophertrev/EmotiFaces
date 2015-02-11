f202ea440c6f6906ff7c6a491099dd12cb085ea6
Christopher Treviño <christophertrev@gmail.com>
2015-02-10 17:21:14 -0800

    (feat) Add https to ajax call

--- client/lib/bundle.js
+++ client/lib/bundle.js
@@ -378,7 +378,7 @@ module.exports = {
   getAllEmotions: function (){
     console.log('getting all emotions')
     $.ajax({
-      url: "http://emotifaces.herokuapp.com/emotion/"
+      url: "https://emotifaces.herokuapp.com/emotion/"
       // url: "http://localhost:3000/emotion"
     })
       .done(function( emotions ) {
--- client/src/utils/webAPIUtils.js
+++ client/src/utils/webAPIUtils.js
@@ -4,7 +4,7 @@ module.exports = {
   getAllEmotions: function (){
     console.log('getting all emotions')
     $.ajax({
-      url: "http://emotifaces.herokuapp.com/emotion/"
+      url: "https://emotifaces.herokuapp.com/emotion/"
       // url: "http://localhost:3000/emotion"
     })
       .done(function( emotions ) {
