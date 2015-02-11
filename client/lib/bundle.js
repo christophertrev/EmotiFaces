cf34c3bf83ae9a5c962a2b9293f71634eeaa0cec
Christopher Treviño <christophertrev@gmail.com>
2015-02-11 14:51:08 -0800

    (refactor) Remove switch statement into objec

--- client/lib/bundle.js
+++ client/lib/bundle.js
@@ -420,73 +420,132 @@ var EmotionsStore = assign({}, EventEmitter.prototype, {
 
 EmotionsStore.dispatchToken = AppDispatcher.register(function(payload) {
   var action = payload.action;
-  console.log(action)
+  console.log(actionList)
+
+  actionList[action.type](action)
   switch(action.type){
 
-    case 'RECEIVE_MESSAGES':
-      // console.log('am in recived RECEIVE_MESSAGES')
-      // console.log(action.rawMessages)
-      for ( var i  in action.rawMessages){
-        _emotions[i] = {
-          id: i,
-          emotion: action.rawMessages[i]
-        }
-      }
-      // console.log(this)
-      EmotionsStore.emitChange();
-      break;
-    case 'SELECT_EMOTION':
-      // this._selectedID = _emotions[action.id].selected;
-      // this._selectedID = action.id
-      // console.log('action???', action.id)
-      _showImages.showLoading = true;
-      _showImages.showEmotion = false;
-      _selectedID = action.id
+    // case 'RECEIVE_MESSAGES':
+    //   // console.log('am in recived RECEIVE_MESSAGES')
+    //   // console.log(action.rawMessages)
+    //   for ( var i  in action.rawMessages){
+    //     _emotions[i] = {
+    //       id: i,
+    //       emotion: action.rawMessages[i]
+    //     }
+    //   }
+    //   // console.log(this)
+    //   EmotionsStore.emitChange();
+    //   break;
+    // case 'SELECT_EMOTION':
+    //   // this._selectedID = _emotions[action.id].selected;
+    //   // this._selectedID = action.id
+    //   // console.log('action???', action.id)
+    //   _showImages.showLoading = true;
+    //   _showImages.showEmotion = false;
+    //   _selectedID = action.id
 
-      console.log('getImgSrc')
-      url = 'http://emotifaces.herokuapp.com/emotion/';
-      if (_emotions[_selectedID]){
-        // console.log(_emotions[_selectedID], 'emotions')
-        url += _emotions[_selectedID].emotion;
-        url += '?timestamp=' + new Date().getTime();
-      } else {
-        //put default image here
-        url = 'img/loading.gif'
-        url= null
-      }
-      _imgURL = url;
-      EmotionsStore.emitChange();
-      break
-    case 'HIDE_LOADING':
-      console.log('hidding loading thing')
-      _showImages.showEmotion = true;
-      _showImages.showLoading = false;
-      EmotionsStore.emitChange();
+    //   console.log('getImgSrc')
+    //   url = 'http://emotifaces.herokuapp.com/emotion/';
+    //   if (_emotions[_selectedID]){
+    //     // console.log(_emotions[_selectedID], 'emotions')
+    //     url += _emotions[_selectedID].emotion;
+    //     url += '?timestamp=' + new Date().getTime();
+    //   } else {
+    //     //put default image here
+    //     url = 'img/loading.gif'
+    //     url= null
+    //   }
+    //   _imgURL = url;
+    //   EmotionsStore.emitChange();
+    //   break
+    // case 'HIDE_LOADING':
+    //   console.log('hidding loading thing')
+    //   _showImages.showEmotion = true;
+    //   _showImages.showLoading = false;
+    //   EmotionsStore.emitChange();
 
-      break
-    case 'REFRESH_IMAGE': 
-      _showImages.showLoading = true;
-      _showImages.showEmotion = false;
-      console.log('REFRESH_IMAGE action!')
-      url = 'http://emotifaces.herokuapp.com/emotion/';
-      if (_emotions[_selectedID]){
-        // console.log(_emotions[_selectedID], 'emotions')
-        url += _emotions[_selectedID].emotion;
-        url += '?timestamp=' + new Date().getTime();
-      } else {
-        //put default image here
-        url = 'img/loading.gif'
-        url= null
-      }
-      _imgURL = url;
-      EmotionsStore.emitChange();
+    // //   break
+    // case 'REFRESH_IMAGE': 
+    //   _showImages.showLoading = true;
+    //   _showImages.showEmotion = false;
+    //   console.log('REFRESH_IMAGE action!')
+    //   url = 'http://emotifaces.herokuapp.com/emotion/';
+    //   if (_emotions[_selectedID]){
+    //     // console.log(_emotions[_selectedID], 'emotions')
+    //     url += _emotions[_selectedID].emotion;
+    //     url += '?timestamp=' + new Date().getTime();
+    //   } else {
+    //     //put default image here
+    //     url = 'img/loading.gif'
+    //     url= null
+    //   }
+    //   _imgURL = url;
+    //   EmotionsStore.emitChange();
 
-      break
+    //   break
 
   }
   
 
 })
+
+var actionList = {
+  RECEIVE_MESSAGES : function (action){
+    for ( var i  in action.rawMessages){
+      _emotions[i] = {
+        id: i,
+        emotion: action.rawMessages[i]
+      }
+    }
+    // console.log(this)
+    EmotionsStore.emitChange();
+  },
+  SELECT_EMOTION : function (action){
+    _showImages.showLoading = true;
+    _showImages.showEmotion = false;
+    _selectedID = action.id
+
+    console.log('getImgSrc')
+    url = 'http://emotifaces.herokuapp.com/emotion/';
+    if (_emotions[_selectedID]){
+      // console.log(_emotions[_selectedID], 'emotions')
+      url += _emotions[_selectedID].emotion;
+      url += '?timestamp=' + new Date().getTime();
+    } else {
+      //put default image here
+      url = 'img/loading.gif'
+      url= null
+    }
+    _imgURL = url;
+    EmotionsStore.emitChange();
+  },
+  HIDE_LOADING : function (action){
+    console.log('hidding loading thing')
+    _showImages.showEmotion = true;
+    _showImages.showLoading = false;
+    EmotionsStore.emitChange();
+  },
+  REFRESH_IMAGE : function (action){
+    _showImages.showLoading = true;
+    _showImages.showEmotion = false;
+    console.log('REFRESH_IMAGE action!')
+    url = 'http://emotifaces.herokuapp.com/emotion/';
+    if (_emotions[_selectedID]){
+      // console.log(_emotions[_selectedID], 'emotions')
+      url += _emotions[_selectedID].emotion;
+      url += '?timestamp=' + new Date().getTime();
+    } else {
+      //put default image here
+      url = 'img/loading.gif'
+      url= null
+    }
+    _imgURL = url;
+    EmotionsStore.emitChange();
+  }
+
+}
+
 
 // webAPIUtils.getAllEmotions();
 
